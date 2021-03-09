d436d7357c74a17823a72468f714797b4dc868bc
KylePersyn <77301598+KylePersyn@users.noreply.github.com>
2021-03-09 14:17:58 -0500

    Added the Person Stub Class

---
+++ Person.java
@@ -0,0 +1,101 @@
+/*
+ * @author Kyle Persyn
+ * This class is going to be the person parent class that witness, victim, and 
+ * person of interest branch off of!
+ */
+public abstract class Person {
+
+	protected String firstName;
+	protected String lastName;
+	protected int age;
+	protected String contactInfo;
+	protected boolean isMinor;
+	
+	/*
+	 * 
+	 */
+	public Person()
+	{
+		
+	}
+	
+	/*
+	 * 
+	 */
+	public String getFirstName()
+	{
+		return firstName;
+	}
+	
+	/*
+	 * 
+	 */
+	public String getLastName()
+	{
+		return lastName;
+	}
+	
+	/*
+	 * 
+	 */
+	public void setFirstName()
+	{
+		
+	}
+	
+	/*
+	 * 
+	 */
+	public void setLastName()
+	{
+		
+	}
+	
+	/*
+	 * 
+	 */
+	public int getAge()
+	{
+		return age;
+	}
+	
+	/*
+	 * 
+	 */
+	public void setAge()
+	{
+		
+	}
+	
+	/*
+	 * 
+	 */
+	public String getContactInfo()
+	{
+			return contactInfo;
+	}
+	
+	/*
+	 * 
+	 */
+	public void setContactInfo()
+	{
+		
+	}
+	
+	/*
+	 * 
+	 */
+	public boolean getIsMinor()
+	{
+		return isMinor;
+	}
+	
+	/*
+	 * 
+	 */
+	public void setIsMinor()
+	{
+		
+	}
+}
