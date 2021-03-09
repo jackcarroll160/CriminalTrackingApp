a9cff575f8a9d6ccc4f63dd43a085578c5da050e
KylePersyn <77301598+KylePersyn@users.noreply.github.com>
2021-03-09 14:44:13 -0500

    Add AccountSearchLogin stub class

---
+++ AccountSearchLogin.java
@@ -0,0 +1,40 @@
+/*
+ * @author Kyle Persyn
+ * This class is going to be the UserActions interface that will assist in searching and editing
+ */
+public class AccountSearchLogin {
+
+	private ArrayList<Admin> admin;
+	private ArrayList<Officer> officer;
+	private ArrayList<RegisteredUser> registereduser;
+	
+	
+	public void promptLoginType()
+	{
+		
+	}
+	
+	
+	public void promptLogin()
+	{
+		
+	}
+	
+	
+	public CrimeList searchCriminal(Criminal criminal)
+	{
+		
+	}
+	
+	
+	public String getLoginType()
+	{
+		
+	}
+	
+	
+	public void setLoginType(String username, String pwd)
+	{
+		
+	}
+}
