3daaaddb1a264dbe86174600c121d9ebd86554c6
Jackson Carroll <Jack Carroll>
2021-03-10 17:01:51 -0500

    Added Evidence Class and Method Stubs

---
+++ Evidence.java
@@ -0,0 +1,60 @@
+public class Evidence {
+    
+    private int idNum;
+    private String dateOfDiscovery;
+    private String description;
+    private String testimony;
+    private String exhibits;
+
+    public String toString() {
+
+    }
+
+    public int getIdNum() {
+        return this.idNum;
+    }
+
+    public void setIdNum(int idnum) {
+
+    }
+
+    public String getDiscoveryDate() {
+        return this.dateOfDiscovery;
+    }
+
+    public void setDiscoveryDate(String date) {
+
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    public void setDescription(String descrip) {
+
+    }
+
+    public String getTestimony() {
+        return this.testimony;
+    }
+
+    public void setTestimony(String testimony) {
+
+    }
+
+    public String getExhibits() {
+        return this.exhibits;
+    }
+
+    public void setExhibits(String exhibit) {
+
+    }
+
+    public void crimeAssociated(Crime crime) {
+
+    }
+
+    private void officerInput(Officer officer) {
+        
+    }
+}
