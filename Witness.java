f19f7e0ad41bd0f6137a1216ce69441363b83905
KylePersyn <77301598+KylePersyn@users.noreply.github.com>
2021-03-15 14:03:45 -0400

    Finished Witness Class

--- Witness.java
+++ Witness.java
@@ -9,80 +9,93 @@ public class Witness extends Person{
 	private String location;
 	private String typeOfWitness;
 	private int witnessId;
-	
+
 	/*
-	 * 
+	 * This is a method that will take all of the data that was input and return it in a readable string format
+	 * @return a String with all of the data and attributes
 	 */
 	public String toString()
 	{
-		return "";
+		return "Proof: " + proof + " \n Testimony: " +testimony +
+				" \n Location: " + location + "\n Type Of Witness: " + typeOfWitness +
+				"\n Witness ID: " + witnessId;
 	}
 
 	/*
-	 * 
+	 * This is the class that will return the proof of the witness
+	 * @return a String that holds the proof
 	 */
 	public String getProof() {
 		return proof;
 	}
 
 	/*
-	 * 
+	 * This is the class that will set the proof of the witness
+	 * @param a String that holds the proof
 	 */
 	public void setProof(String proof) {
 		this.proof = proof;
 	}
 
 	/*
-	 * 
+	 * This is the class that will return the testimony of the witness
+	 * @return a String that holds the testimony
 	 */
 	public String getTestimony() {
 		return testimony;
 	}
 
 	/*
-	 * 
+	 * This is the class that will set the testimony of the witness
+	 * @param a String that holds the testimony
 	 */
 	public void setTestimony(String testimony) {
 		this.testimony = testimony;
 	}
 
 	/*
-	 * 
+	 * This is the class that will return the location of the witness
+	 * @return a String that holds the location
 	 */
 	public String getLocation() {
 		return location;
 	}
 
 	/*
-	 * 
+	 * This is the class that will set the location of the witness
+	 * @param a String that holds the location
 	 */
 	public void setLocation(String location) {
 		this.location = location;
 	}
 
 	/*
-	 * 
+	 * This is the class that will return the type of the witness
+	 * @return a String that holds the type
 	 */
 	public String getTypeOfWitness() {
 		return typeOfWitness;
 	}
 
 	/*
-	 * 
+	 * This is the class that will set the type of the witness
+	 * @param a String that holds the type
 	 */
 	public void setTypeOfWitness(String typeOfWitness) {
 		this.typeOfWitness = typeOfWitness;
 	}
 
 	/*
-	 * 
+	 * This is the class that will return the id of the witness
+	 * @return an int that holds the id
 	 */
 	public int getWitnessId() {
 		return witnessId;
 	}
 
 	/*
-	 * 
+	 * This is the class that will set the id of the witness
+	 * @param an int that holds the id
 	 */
 	public void setWitnessId(int witnessId) {
 		this.witnessId = witnessId;
