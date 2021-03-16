5d051823522f994850c0cdd203a98f5baaf3aa7e
soklota <oklotasydney@gmail.com>
2021-03-16 14:56:42 -0400

    first and last name added

--- Criminal.java
+++ Criminal.java
@@ -35,6 +35,15 @@ public class Criminal {
 
     }
 
+    public String getFirstName(){
+
+        return this.firstName;
+    }
+
+    public String getLastName(){
+
+        return this.lastName;
+    }
     public ArrayList<UUID> getID(){
 
         return this.ID;
@@ -145,6 +154,14 @@ public class Criminal {
 
     }
 
+    public void setFirstName(String firstname){
+
+    }
+
+    public void setLastName(String lastname){
+
+    }
+    
     public void setCriminalID(int criminalID){
 
     }
