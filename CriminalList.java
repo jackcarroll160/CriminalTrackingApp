ecddefb40eab3fe1e31e3d5722c829ebf82956d0
KylePersyn <77301598+KylePersyn@users.noreply.github.com>
2021-03-16 14:59:11 -0400

    Add files via upload

--- CriminalList.java
+++ CriminalList.java
@@ -1,42 +1,66 @@
+import java.util.ArrayList;
+import java.util.UUID;
+
+
 /*
  * @author Kyle Persyn
  * This class is going to be the CriminalList  class that will hold all of the criminals
  */
 public class CriminalList implements UserActions{
 
-	private ArrayList<Criminal> criminalList;
-	private Criminal criminal;
+	private static ArrayList<Criminal> criminalList = new ArrayList<Criminal>();
+	private static Criminal criminal = null;
 	
 	/*
-	 * 
+	 * This is the constructor where the criminal array list will be constructed
 	 */
-	public CriminalList()
+	private CriminalList()
 	{
-		
+		criminalList = new DataLoader.loadCriminals();
 	}
 	
 	/*
-	 * 
+	 * This is the getInstance method that will create a new criminal as long as criminal List is not null
+	 * @return An instance of library
 	 */
-	public Criminal getInstance()
+	public static Criminal getInstance()
 	{
+		if (criminal == null) {
+			criminal = new Criminal();
+		}
 		return criminal;
 	}
 	
 	/*
 	 * 
 	 */
-	public ArrayList<Criminal> getCriminals(String fullName)
+	public ArrayList<Criminal> getCriminals()
 	{
 		return criminalList;
 	}
+	
 	
 	/*
 	 * 
 	 */
 	public void searchCriminalsByName(String Fname, String Lname)
 	{
+		int searchLength = criminalList.size();
+		boolean found = false;
 		
+		for (int i = 0; i < searchLength; i++)
+		{
+			if(criminalList.get(i) != null && criminalList.get(i).getFirstName() == Fname &&criminalList.get(i).getLastName() == Lname)
+			{
+				System.out.println(criminalList.get(i).toString());
+				found = true;
+			}
+		}
+		
+		if (!found)
+		{
+			System.out.println("The Criminal entered was not found in our database!");
+		}
 	}
 
 	/*
