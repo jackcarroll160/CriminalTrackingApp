6148cfcaa673b8e883f40744e5301948db864dbb
soklota <oklotasydney@gmail.com>
2021-03-15 23:59:05 -0400

    method edits

--- UserInterface.java
+++ UserInterface.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 /**
  * Author: Sydney Oklota
  */
@@ -7,17 +9,7 @@ public interface UserInterface {
     
     public void editCriminalByName(String Fname, String Lname);
 
-    public void registerAdmin(Admin admin);
 
-    public void registerOfficer(Officer officer);
-
-    public void registerUser(RegisteredUser user);
-
-    public void removeAdmin(Admin admin);
-
-    public void removeOfficer(Officer officer);
-
-    public void removeUser(RegisteredUser user);
 
 
 
