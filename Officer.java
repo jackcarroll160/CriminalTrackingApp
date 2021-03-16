af7a741aecce743728a7f1cc8e067a3df31f46b2
soklota <oklotasydney@gmail.com>
2021-03-15 23:57:30 -0400

    method edits, almost complete

--- Officer.java
+++ Officer.java
@@ -2,37 +2,87 @@
  * Author: Sydney Okota
  */
 public class Officer extends User {
-
-    private String rank;
-
-    public Officer(){
-
-        this.rank = rank;
-
-    }
-
-    public void addMisdemeanors(Criminal criminal){
+    
+    
+    
+    public Officer(String username, String password, String rank, int authorizationLevel){
+        super(username, password, authorizationLevel);
+        
 
     }
 
-    public void addFelonies(Criminal criminal){
+
+    /**
+     * add misdemeanors to crime arraylist
+     * @param crime array list
+     * @param misdemeanor misdemeanor being added
+     */
+    public void addMisdemeanors(Criminal crime, String misdemeanor){
+        crime.add(misdemeanor);
 
     }
 
-    public void addReport(Criminal criminal){
+    /**
+     * add felonies to crime arraylist
+     * @param crime array list
+     * @param felony felony being added
+     */
+    public void addFelonies(Criminal crime, String felony){
+        crime.add(felony);
 
     }
 
-    public void editReport(Criminal criminal){
+    /**
+     * remove misdemeanor from array list
+     * @param crime array list
+     * @param misdemeanor misdemeanor being removed
+     */
+    public void removeMisdemeanors(Criminal crime, String misdemeanor){
+        crime.remove(misdemeanor);
 
     }
 
-    public void removeMisdemeanors(Criminal criminal){
+    /**
+     * remove felony from array list
+     * @param crime array list
+     * @param felony felony being removed
+     */
+    public void removeFelonies(Criminal crime, String felony){
+        crime.remove(felony);
+    }
+
+
+
+
+
+
+
+    //should keep? what are they exactly?
+
+    public void addReport(Crime crime){
+        
 
     }
 
-    public void removeFelonies(Criminal criminal){
+    public void editReport(Crime crime){
 
+    }
+
+
+
+    
+
+    @Override
+    public void searchCriminalsByName(String Fname, String Lname) {
+        // TODO Auto-generated method stub
+        
+    }
+
+
+    @Override
+    public void editCriminalByName(String Fname, String Lname) {
+        // TODO Auto-generated method stub
+        
     }
     
 }
