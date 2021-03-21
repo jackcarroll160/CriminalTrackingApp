420779010743fe95c8d667bc9c5cd566f628517d
rosesatriano <satrianr@email.sc.edu>
2021-03-21 12:27:41 -0400

    data loader

--- DataLoader.java
+++ DataLoader.java
@@ -7,6 +7,34 @@ import org.json.simple.parser.JSONParser;
 
 public class DataLoader extends DataConstants{
 
+// crime
+public static ArrayList <Crime> loadCrime(){
+    ArrayList <Crime> crime = new ArrayList <Crime>();
+
+    try{
+        FileReader reader = new FileReader(CRIME_FILE_NAME);
+        JSONParser parser = new JSONParser();
+        JSONArray crimeJSON = (JSONArray) new JSONParser().parse(reader);
+        
+
+        for (int i = 0; i < crimeJSON.size(); i++){
+            JSONObject crimeJSON = (JSONObject)crimeJSON.get(i);
+            String description = (String)crimeJSON.get(CRIME_DESCRIPTION);
+            String dateOfCrime = (String)crimeJSON.get(CRIME_DATE_OF_CRIME);
+            String timeOfCrime = (String)crimeJSON.get(CRIME_TIME_OF_CRIME);
+            String officers = (String)crimeJSON.get(CRIME_OFFICERS);
+            crime.add(new Crime(description, dateOfCrime, timeOfCrime, officers));
+        }
+
+        return crime;
+    } catch (Exception e){
+        e.printStackTrace();
+    }
+
+    return null;
+}
+
+
     // criminal
     public static ArrayList <Criminal> loadCriminals(){
         ArrayList <Criminal> criminal = new ArrayList <Criminal>();
@@ -21,9 +49,13 @@ public class DataLoader extends DataConstants{
                 JSONObject criminalJSON = (JSONObject)criminalJSON.get(i);
                 String firstName = (String)criminalJSON.get(CRIMINALS_FIRST_NAME);
                 String lastName = (String)criminalJSON.get(CRIMINALS_LAST_NAME);
+                String nickname = (String)criminalJSON.get(CRIMINALS_NICKNAME);
                 int age = ((Long)criminalJSON.get(CRIMINALS_AGE)).intValue();
+                String ethnicity = (String)criminalJSON.get(CRIMINALS_ETHNICITY);
+                String contactinfo = (String)criminalJSON.get(CRIMINALS_CONTACT_INFO);
+                String linkedCrime = (String)criminalJSON.get(CRIMINALS_LINKED_CRIME);
 
-                criminal.add(new Criminal(firstName,lastName));
+                criminal.add(new Criminal(firstName,lastName, nickname, age, gender, ethnicity, contactinfo, linkedCrime));
             }
 
             return criminal;
@@ -35,32 +67,7 @@ public class DataLoader extends DataConstants{
         return null;
     }
 
-    // crime
-    public static ArrayList <Crime> loadCrime(){
-        ArrayList <Crime> crime = new ArrayList <Crime>();
-
-        try{
-            FileReader reader = new FileReader(CRIME_FILE_NAME);
-            JSONParser parser = new JSONParser();
-            JSONArray crimeJSON = (JSONArray) new JSONParser().parse(reader);
-            
-
-            for (int i = 0; i < crimeJSON.size(); i++){
-                JSONObject crimeJSON = (JSONObject)crimeJSON.get(i);
-                String description = (String)crimeJSON.get(CRIME_DESCRIPTION);
-                String dateOfCrime = (String)crimeJSON.get(CRIME_DATE_OF_CRIME);
-
-                crime.add(new Crime(description, dateOfCrime));
-            }
-
-            return crime;
-        } catch (Exception e){
-            e.printStackTrace();
-        }
-
-        return null;
-    }
-
+    
     // person of interest
     public static ArrayList <POI> loadPOI(){
         ArrayList <POI> poi = new ArrayList <POI>();
