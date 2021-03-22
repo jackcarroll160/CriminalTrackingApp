957b239a94aa4742cf7216f4f36cc483b68eb915
Kyle Persyn <kpersyn@email.sc.edu>
2021-03-22 17:42:30 -0400

    Updated Criminal Class

--- Criminal.java
+++ Criminal.java
@@ -6,268 +6,292 @@ public class Criminal {
     
     private ArrayList<UUID> ID;
     private int criminalID;
-    private boolean hasNickname;
-    private String nickname;
-    private String knownAddress;
-    private ArrayList<String> affiliates;
-    private ArrayList<String> familyMembers;
-    private double shoeSize;
-    private ArrayList<Crime> crime;
-    private String crimeDescription;
-    private boolean victimAlive;
-    private String hairColor;
-    private String clothing;
-    private String tattoo;
-    private String facialHair;
-    private String witnessData;
-    private boolean isViolent;
-    private String gangAffiliation;
-    private String dateOfCrime;
-    private boolean hasWitness;
-    private Witness witness;
-    private boolean hasFingerprints;
-    private String criminalReport;
     private String firstName;
     private String lastName;
+    private boolean hasNickname;
+    private String nickname;
+    private int age;
+    private String gender;
+    private String ethnicity;
+    private String contactinfo;
+    private String linkedCrime;
+    private int crimeId;
+    private String birthDate;
+    private String eyeColor;
+    private String hairDescription;
+    private Boolean facialHair;
+    private String facialHairDescription;
+    private String height;
+    private String weight;
+    private Boolean hasTattoos;
+    private String tattooDescription;
+    private Boolean hasPiercings;
+    private String physicalMarksDescription;
+    private String physicalBuild;
+    private Boolean hasWeapon;
+    private String weaponDescription;
+    private String complexion;
+    private String clothingDescription;
+    private Boolean isGuilty;
+    private Boolean hasFingerprints;
+    private Boolean hasVehicle;
+    private String vehicleDescription;
 
 
-    /**
-     * getters
-     */
-    public String getHairColor()
-    {
-        return this.hairColor;
-    }
-    
-    public String getClothing()
-    {
-        return this.clothing;
-    }
-    public String getTattoo()
-    {
-        return this.tattoo;
-    }
-
-    public String getFacialHair()
-    {
-        return this.facialHair;
-    }
-
-    public String toString(){
-        return null;
-
-    }
-
-    public String getFirstName(){
-
-        return this.firstName;
-    }
-
-    public String getLastName(){
-
-        return this.lastName;
-    }
-    public ArrayList<UUID> getID(){
-
-        return this.ID;
-
+    public ArrayList<UUID> getID() {
+        return ID;
     }
 
-    public int getCriminalID(){
-
-        return this.criminalID;
-
+    public void setID(ArrayList<UUID> iD) {
+        ID = iD;
     }
 
-    public String getNickname(){
-
-        return this.nickname;
-
+    public int getCriminalID() {
+        return criminalID;
     }
 
-    public String getKnownAddress(){
-
-        return this.knownAddress;
-
-    }
-
-    public ArrayList<String> getAffiliates(){
-
-        return this.affiliates;
-
-    }
-
-    public ArrayList<String> getFamilyMembers(){
-
-        return this.familyMembers;
-
-    }
-
-    public double getShoeSize(){
-
-        return this.shoeSize;
-
-    }
-
-    public ArrayList<Crime> getCrime(){
-
-        return this.crime;
-
-    }
-
-    public String getCrimeDescription(){
-
-        return this.crimeDescription;
-
-    }
-
-    public boolean getVictimStatus(){
-
-        return this.victimAlive;
-
-    }
-
-    public String getWitnessData(){
-
-        return this.witnessData;
-
-    }
-
-    public boolean getViolence(){
-
-        return this.isViolent;
-
-    }
-    
-    public String getGangAffiliation(){
-
-        return this.gangAffiliation;
-
-    }
-
-    public String getDateOfCrime(){
-
-        return this.dateOfCrime;
-
-    }
-
-    public Witness getWitness(){
-
-        return this.witness;
-
-    }
-
-    public boolean getHasFingerprints(){
-
-        return this.hasFingerprints;
-
-    }
-
-    public boolean getHasNickname(){
-
-        return this.hasNickname;
-
-    }
-
-
-    /**
-     * setters
-     */
-    public void setHairColor(String hairColor)
-    {
-        this.hairColor = hairColor;
-    }
-    
-    public void setClothing(String clothing)
-    {
-        this.clothing = clothing;
-    }
-    public void setTattoo(String tattoo)
-    {
-        this.tattoo = tattoo;
-    }
-
-    public void setFacialHair(String facialHair)
-    {
-        this.facialHair = facialHair;
-    }
-
-    public void setID(ArrayList<UUID> ID){
-        this.ID = ID;
-    }
-
-    public void setFirstName(String firstname){
-        this.firstName = firstname;
-    }
-
-    public void setLastName(String lastname){
-        this.lastName = lastname;
-    }
-
-    public void setCriminalID(int criminalID){
+    public void setCriminalID(int criminalID) {
         this.criminalID = criminalID;
     }
 
-    public void setNickname(String nickname){
-        this.nickname = nickname;
+    public String getFirstName() {
+        return firstName;
     }
 
-    public void setKnownAddress(String knownAddress){
-        this.knownAddress = knownAddress;
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
     }
 
-    public void setAffiliates(ArrayList<String> affiliates){
-        this.affiliates = affiliates;
+    public String getLastName() {
+        return lastName;
     }
 
-    public void setFamilyMembers(ArrayList<String> familyMembers){
-        this.familyMembers = familyMembers;
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
     }
 
-    public void setShoeSize(double shoeSize){
-        this.shoeSize = shoeSize;
+    public boolean isHasNickname() {
+        return hasNickname;
     }
 
-    public void setCrime(ArrayList<Crime> crime){
-        this.crime = crime;
-    }
-
-    public void setCrimeDescription(String crimeDescription){
-        this.crimeDescription = crimeDescription;
-    }
-
-    public void setVictimStatus(boolean victimAlive){
-        this.victimAlive = victimAlive;
-    }
-
-    public void setWitnessData(String witnessData){
-        this.witnessData = witnessData;
-    }
-
-    public void setViolence(boolean isViolent){
-        this.isViolent = isViolent;
-    }
-
-    public void setGangAffiliation(String gangAffiliation){
-        this.gangAffiliation = gangAffiliation;
-    }
-
-    public void setDateOfCrime(String dateOfCrime){
-        this.dateOfCrime = dateOfCrime;
-    }
-
-    public void setWitness(Witness witness){
-        this.witness = witness;
-    }
-    
-    public void setHasFingerprints(boolean hasFingerprints){
-        this.hasFingerprints = hasFingerprints;
-    }
-
-    public void setHasNickname(boolean hasNickname){
+    public void setHasNickname(boolean hasNickname) {
         this.hasNickname = hasNickname;
     }
 
+    public String getNickname() {
+        return nickname;
+    }
 
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getEthnicity() {
+        return ethnicity;
+    }
+
+    public void setEthnicity(String ethnicity) {
+        this.ethnicity = ethnicity;
+    }
+
+    public String getContactinfo() {
+        return contactinfo;
+    }
+
+    public void setContactinfo(String contactinfo) {
+        this.contactinfo = contactinfo;
+    }
+
+    public String getLinkedCrime() {
+        return linkedCrime;
+    }
+
+    public void setLinkedCrime(String linkedCrime) {
+        this.linkedCrime = linkedCrime;
+    }
+
+    public int getCrimeId() {
+        return crimeId;
+    }
+
+    public void setCrimeId(int crimeId) {
+        this.crimeId = crimeId;
+    }
+
+    public String getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(String birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public String getEyeColor() {
+        return eyeColor;
+    }
+
+    public void setEyeColor(String eyeColor) {
+        this.eyeColor = eyeColor;
+    }
+
+    public String getHairDescription() {
+        return hairDescription;
+    }
+
+    public void setHairDescription(String hairDescription) {
+        this.hairDescription = hairDescription;
+    }
+
+    public Boolean getFacialHair() {
+        return facialHair;
+    }
+
+    public void setFacialHair(Boolean facialHair) {
+        this.facialHair = facialHair;
+    }
+
+    public String getFacialHairDescription() {
+        return facialHairDescription;
+    }
+
+    public void setFacialHairDescription(String facialHairDescription) {
+        this.facialHairDescription = facialHairDescription;
+    }
+
+    public String getHeight() {
+        return height;
+    }
+
+    public void setHeight(String height) {
+        this.height = height;
+    }
+
+    public String getWeight() {
+        return weight;
+    }
+
+    public void setWeight(String weight) {
+        this.weight = weight;
+    }
+
+    public Boolean getHasTattoos() {
+        return hasTattoos;
+    }
+
+    public void setHasTattoos(Boolean hasTattoos) {
+        this.hasTattoos = hasTattoos;
+    }
+
+    public String getTattooDescription() {
+        return tattooDescription;
+    }
+
+    public void setTattooDescription(String tattooDescription) {
+        this.tattooDescription = tattooDescription;
+    }
+
+    public Boolean getHasPiercings() {
+        return hasPiercings;
+    }
+
+    public void setHasPiercings(Boolean hasPiercings) {
+        this.hasPiercings = hasPiercings;
+    }
+
+    public String getPhysicalMarksDescription() {
+        return physicalMarksDescription;
+    }
+
+    public void setPhysicalMarksDescription(String physicalMarksDescription) {
+        this.physicalMarksDescription = physicalMarksDescription;
+    }
+
+    public String getPhysicalBuild() {
+        return physicalBuild;
+    }
+
+    public void setPhysicalBuild(String physicalBuild) {
+        this.physicalBuild = physicalBuild;
+    }
+
+    public Boolean getHasWeapon() {
+        return hasWeapon;
+    }
+
+    public void setHasWeapon(Boolean hasWeapon) {
+        this.hasWeapon = hasWeapon;
+    }
+
+    public String getWeaponDescription() {
+        return weaponDescription;
+    }
+
+    public void setWeaponDescription(String weaponDescription) {
+        this.weaponDescription = weaponDescription;
+    }
+
+    public String getComplexion() {
+        return complexion;
+    }
+
+    public void setComplexion(String complexion) {
+        this.complexion = complexion;
+    }
+
+    public String getClothingDescription() {
+        return clothingDescription;
+    }
+
+    public void setClothingDescription(String clothingDescription) {
+        this.clothingDescription = clothingDescription;
+    }
+
+    public Boolean getIsGuilty() {
+        return isGuilty;
+    }
+
+    public void setIsGuilty(Boolean isGuilty) {
+        this.isGuilty = isGuilty;
+    }
+
+    public Boolean getHasFingerprints() {
+        return hasFingerprints;
+    }
+
+    public void setHasFingerprints(Boolean hasFingerprints) {
+        this.hasFingerprints = hasFingerprints;
+    }
+
+    public Boolean getHasVehicle() {
+        return hasVehicle;
+    }
+
+    public void setHasVehicle(Boolean hasVehicle) {
+        this.hasVehicle = hasVehicle;
+    }
+
+    public String getVehicleDescription() {
+        return vehicleDescription;
+    }
+    
+    public void setVehicleDescription(String vehicleDescription) {
+        this.vehicleDescription = vehicleDescription;
+    }
 
 }
