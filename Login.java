4fb608cc57dc5c0be22c25a22b401f1f253cb1ba
Jack Carroll <jhc8@email.sc.edu>
2021-03-24 19:06:37 -0400

    Attempted to implement JSON

--- Login.java
+++ Login.java
@@ -1,61 +1,35 @@
 //package GUI_2;
 
+import java.io.IOException;
 import java.util.HashMap;
-import java.util.Map;
-
-import javax.swing.JOptionPane;
-
-import java.awt.*;
 
 public class Login extends LoginDialog {
 
-    private static HashMap<String, String> loginInfo = new HashMap<>();
-    private boolean usernameTaken;
+    //private static HashMap<String, String> loginInfo = new HashMap<>();
  
-    public static boolean authenticate(String username, String password) {
-
-        loginInfo.put("user1", "password1");
-        loginInfo.put("user2", "password2");
-        //loginInfo.put("user3", "password3");
-        //loginInfo.put("user4", "password4");
-        //loginInfo.put("user5", "password5");
-        //loginInfo.put("", ""); // for easy login for me
+    public static boolean authenticate(String username, String password) throws IOException {
+        Users userList = new Users();
+        //loginInfo.put("user1", "password1");
+        //loginInfo.put("user2", "password2");
 
         // verifying login credentials 
-        if (loginInfo.containsKey(username) && loginInfo.get(username).equals(password)) {
+        /*if (loginInfo.containsKey(username) && loginInfo.get(username).equals(password)) {
             return true;
         }
         else {
             loginInfo.put(username, password);
             return false;
-        }
-
-    }
-
-    public static boolean authenticateNewUser(String username, String password) {
-        loginInfo.put("user1", "password1");
-        loginInfo.put("user2", "password2");
-        
-        if (loginInfo.containsKey(username)) {
-            return false;
-        }
-        else {
-            loginInfo.put(username, password);
+        } */
+        if (userList.haveUser(username)) {
             return true;
         }
+        return false;
+
     }
 
-    private boolean checkAllUsernames(String username, String password) {
-        for (Map.Entry<String, String> entry : loginInfo.entrySet()) {
-            String key = entry.getKey();
-            String value = entry.getValue();
-            if (key.equalsIgnoreCase(username))
-                usernameTaken = true;
-            else if (key.equalsIgnoreCase(username) && value.equals(password))
-                return true;
-            else
-                continue;
-        }
+    public static boolean authenticateNewUser(String username, String password) throws IOException {
+        Users userList = new Users();
+        return userList.addUser(username, password);
     }
 
 }
