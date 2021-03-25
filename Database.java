21429937f5bf7320af2aa26a9bc3ff032cf42d58
rosesatriano <satrianr@email.sc.edu>
2021-03-25 12:26:14 -0400

    add database class

---
+++ Database.java
@@ -0,0 +1,48 @@
+import java.util.ArrayList;
+
+
+public class Database {
+
+    // crime
+    public static ArrayList<Crime> getCrime() {
+        return new ArrayList<Crime>();
+
+    }
+
+    // criminal
+    public static ArrayList<Criminal> getCriminals() {
+        return new ArrayList<Criminal>();
+
+    }
+    
+    // poi
+    public static ArrayList<PersonOfInterest> getPOI() {
+        return new ArrayList<PersonOfInterest>();
+
+    }
+
+    // suspects
+    public static ArrayList<Suspect> getSuspect() {
+        return new ArrayList<Suspect>();
+
+    }
+
+    // user
+    public static ArrayList<User> getUserList() {
+        return new ArrayList<User>();
+
+    }
+
+    // victim
+    public static ArrayList<Victim> getVictims() {
+        return new ArrayList<Victim>();
+
+    }
+
+    // witness
+    public static ArrayList<Witness> getWitnesses() {
+        return new ArrayList<Crime>();
+
+    }
+
+}
