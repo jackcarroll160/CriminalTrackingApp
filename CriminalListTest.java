0f1203e733c5f46b444c9bbf912360c444a59b1c
Kyle Persyn <kpersyn@email.sc.edu>
2021-04-06 14:49:07 -0400

    Finished CriminalList Test

--- CriminalListTest.java
+++ CriminalListTest.java
@@ -119,4 +119,207 @@ class CriminalListTest {
 		boolean hasEthnicity = secondCrim.getContactinfo().equalsIgnoreCase("803-298-7924");
 		assertTrue(hasEthnicity);
 	}
+
+	@Test
+	void testHaveFirstCriminalValidLinkedCrime() {
+        Criminal firstCrim = criminalList.get(0);
+		boolean hasEthnicity = firstCrim.getLinkedCrime().equalsIgnoreCase("Speeding");
+		assertTrue(hasEthnicity);
+	}
+
+	@Test
+	void testHaveSecondCriminalValidLinkedCrime() {
+        Criminal secondCrim = criminalList.get(1);
+		boolean hasEthnicity = secondCrim.getLinkedCrime().equalsIgnoreCase("Trespassing");
+		assertTrue(hasEthnicity);
+	}
+
+	@Test
+	void testHaveFirstCriminalValidBirthDate() {
+        Criminal firstCrim = criminalList.get(0);
+		boolean hasEthnicity = firstCrim.getBirthDate().equalsIgnoreCase("July 24, 2001");
+		assertTrue(hasEthnicity);
+	}
+
+	@Test
+	void testHaveSecondCriminalValidBirthDate() {
+        Criminal secondCrim = criminalList.get(1);
+		boolean hasEthnicity = secondCrim.getBirthDate().equalsIgnoreCase("March 17, 2001");
+		assertTrue(hasEthnicity);
+	}
+
+	@Test
+	void testHaveFirstCriminalValidEyeColor() {
+        Criminal firstCrim = criminalList.get(0);
+		boolean hasEthnicity = firstCrim.getEyeColor().equalsIgnoreCase("Green");
+		assertTrue(hasEthnicity);
+	}
+
+	@Test
+	void testHaveSecondCriminalValidEyeColor() {
+        Criminal secondCrim = criminalList.get(1);
+		boolean hasEthnicity = secondCrim.getEyeColor().equalsIgnoreCase("Hazel");
+		assertTrue(hasEthnicity);
+	}
+
+	@Test
+	void testHaveFirstCriminalValidHair() {
+        Criminal firstCrim = criminalList.get(0);
+		boolean hasEthnicity = firstCrim.getHairDescription().equalsIgnoreCase("Short Brown Hair");
+		assertTrue(hasEthnicity);
+	}
+
+	@Test
+	void testHaveSecondCriminalValidHair() {
+        Criminal secondCrim = criminalList.get(1);
+		boolean hasEthnicity = secondCrim.getHairDescription().equalsIgnoreCase("Medium Length Brown Hair");
+		assertTrue(hasEthnicity);
+	}
+
+	@Test
+	void testHaveFirstCriminalValidFacialHair() {
+        Criminal firstCrim = criminalList.get(0);
+		boolean hasEthnicity = firstCrim.getFacialHairDescription().equalsIgnoreCase("Beard");
+		assertTrue(hasEthnicity);
+	}
+
+	@Test
+	void testHaveSecondCriminalValidFacialHair() {
+        Criminal secondCrim = criminalList.get(1);
+		boolean hasEthnicity = secondCrim.getFacialHairDescription().equalsIgnoreCase("No Facial Hair");
+		assertTrue(hasEthnicity);
+	}
+
+	@Test
+	void testHaveFirstCriminalValidHeight() {
+        Criminal firstCrim = criminalList.get(0);
+		boolean hasEthnicity = firstCrim.getHeight().equalsIgnoreCase("5'11");
+		assertTrue(hasEthnicity);
+	}
+
+	@Test
+	void testHaveSecondCriminalValidHeight() {
+        Criminal secondCrim = criminalList.get(1);
+		boolean hasEthnicity = secondCrim.getHeight().equalsIgnoreCase("5'5");
+		assertTrue(hasEthnicity);
+	}
+
+	@Test
+	void testHaveFirstCriminalValidWeight() {
+        Criminal firstCrim = criminalList.get(0);
+		boolean hasEthnicity = firstCrim.getWeight().equalsIgnoreCase("178");
+		assertTrue(hasEthnicity);
+	}
+
+	@Test
+	void testHaveSecondCriminalValidWeight() {
+        Criminal secondCrim = criminalList.get(1);
+		boolean hasEthnicity = secondCrim.getWeight().equalsIgnoreCase("130");
+		assertTrue(hasEthnicity);
+	}
+
+	@Test
+	void testHaveFirstCriminalValidTattoo() {
+        Criminal firstCrim = criminalList.get(0);
+		boolean hasEthnicity = firstCrim.getTattooDescription().equalsIgnoreCase("No Tattoos");
+		assertTrue(hasEthnicity);
+	}
+
+	@Test
+	void testHaveSecondCriminalValidTattoo() {
+        Criminal secondCrim = criminalList.get(1);
+		boolean hasEthnicity = secondCrim.getTattooDescription().equalsIgnoreCase("No Tattoos");
+		assertTrue(hasEthnicity);
+	}
+
+	@Test
+	void testHaveFirstCriminalValidMarks() {
+        Criminal firstCrim = criminalList.get(0);
+		boolean hasEthnicity = firstCrim.getPhysicalMarksDescription().equalsIgnoreCase("No Physical Marks");
+		assertTrue(hasEthnicity);
+	}
+
+	@Test
+	void testHaveSecondCriminalValidMarks() {
+        Criminal secondCrim = criminalList.get(1);
+		boolean hasEthnicity = secondCrim.getPhysicalMarksDescription().equalsIgnoreCase("No Physical Marks");
+		assertTrue(hasEthnicity);
+	}
+
+	
+	@Test
+	void testHaveFirstCriminalValidBuild() {
+        Criminal firstCrim = criminalList.get(0);
+		boolean hasEthnicity = firstCrim.getPhysicalBuild().equalsIgnoreCase("Athletic");
+		assertTrue(hasEthnicity);
+	}
+
+	@Test
+	void testHaveSecondCriminalValidBuild() {
+        Criminal secondCrim = criminalList.get(1);
+		boolean hasEthnicity = secondCrim.getPhysicalBuild().equalsIgnoreCase("Athletic");
+		assertTrue(hasEthnicity);
+	}
+	@Test
+	void testHaveFirstCriminalValidWeapon() {
+        Criminal firstCrim = criminalList.get(0);
+		boolean hasEthnicity = firstCrim.getWeaponDescription().equalsIgnoreCase("No Weapon");
+		assertTrue(hasEthnicity);
+	}
+
+	@Test
+	void testHaveSecondCriminalValidWeapon() {
+        Criminal secondCrim = criminalList.get(1);
+		boolean hasEthnicity = secondCrim.getWeaponDescription().equalsIgnoreCase("No Weapon");
+		assertTrue(hasEthnicity);
+	}
+	
+	@Test
+	void testHaveFirstCriminalValidComplexion() {
+        Criminal firstCrim = criminalList.get(0);
+		boolean hasEthnicity = firstCrim.getComplexion().equalsIgnoreCase("Smooth Tan Complexion");
+		assertTrue(hasEthnicity);
+	}
+
+	@Test
+	void testHaveSecondCriminalValidComplexion() {
+        Criminal secondCrim = criminalList.get(1);
+		boolean hasEthnicity = secondCrim.getComplexion().equalsIgnoreCase("Smooth Tan Complexion");
+		assertTrue(hasEthnicity);
+	}
+	
+	@Test
+	void testHaveFirstCriminalValidClothing() {
+        Criminal firstCrim = criminalList.get(0);
+		boolean hasEthnicity = firstCrim.getClothingDescription().equalsIgnoreCase("Nike Gym Wear");
+		assertTrue(hasEthnicity);
+	}
+
+	@Test
+	void testHaveSecondCriminalValidClothing() {
+        Criminal secondCrim = criminalList.get(1);
+		boolean hasEthnicity = secondCrim.getClothingDescription().equalsIgnoreCase("Yoga Athletic Wear");
+		assertTrue(hasEthnicity);
+	}
+	
+	@Test
+	void testHaveFirstCriminalValidVehicle() {
+        Criminal firstCrim = criminalList.get(0);
+		boolean hasEthnicity = firstCrim.getVehicleDescription().equalsIgnoreCase("Black Chevy Avalanche");
+		assertTrue(hasEthnicity);
+	}
+
+	@Test
+	void testHaveSecondCriminalValidVehicle() {
+        Criminal secondCrim = criminalList.get(1);
+		boolean hasEthnicity = secondCrim.getVehicleDescription().equalsIgnoreCase("Gray Camry");
+		assertTrue(hasEthnicity);
+	}
+
+	@Test
+	void testLengthOfList()
+	{
+		assertEquals(criminalList.size(), 2);
+	}
+
 }
