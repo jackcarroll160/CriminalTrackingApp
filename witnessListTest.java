eb30ebc06b9efa5f7bd53949d090df10f59bda78
Kyle Persyn <kpersyn@email.sc.edu>
2021-04-06 17:59:27 -0400

    Finished WitnessListTest

--- witnessListTest.java
+++ witnessListTest.java
@@ -78,6 +78,88 @@ class witnessListTest {
 		assertTrue(hasContact);
 	}
 
+    @Test
+	void testHaveFirstWitnessValidMinor() {
+        Witness firstWitness = WitnessList.get(0);
+		boolean isMinor = firstWitness.getIsMinor().equalsIgnoreCase("False");
+		assertTrue(isMinor);
+	}
 
+	@Test
+	void testHaveSecondWitnessValidMinor() {
+        Witness secondWitness = WitnessList.get(1);
+		boolean isMinor = secondWitness.getIsMinor().equalsIgnoreCase("False");
+		assertTrue(isMinor);
+	}
+
+    @Test
+	void testHaveFirstWitnessValidProof() {
+        Witness firstWitness = WitnessList.get(0);
+		boolean Proof = firstWitness.getProof().equalsIgnoreCase("I found the suspects jacket on the floor");
+		assertTrue(Proof);
+	}
+
+	@Test
+	void testHaveSecondWitnessValidProof() {
+        Witness secondWitness = WitnessList.get(1);
+		boolean Proof = secondWitness.getProof().equalsIgnoreCase("No Proof");
+		assertTrue(Proof);
+	}
+
+    @Test
+	void testHaveFirstWitnessValidTestimony() {
+        Witness firstWitness = WitnessList.get(0);
+		boolean hasTestimony = firstWitness.getTestimony().equalsIgnoreCase("I was fueling my car when the suspect stole the purse");
+		assertTrue(hasTestimony);
+	}
+
+	@Test
+	void testHaveSecondWitnessValidTestimony() {
+        Witness secondWitness = WitnessList.get(1);
+		boolean hasTestimony = secondWitness.getTestimony().equalsIgnoreCase("I was in my room during the crime and watched it from window");
+		assertTrue(hasTestimony);
+	}
+
+    @Test
+	void testHaveFirstWitnessValidLocation() {
+        Witness firstWitness = WitnessList.get(0);
+		boolean hasLocation = firstWitness.getLocation().equalsIgnoreCase("Shell Gas Station");
+		assertTrue(hasLocation);
+	}
+
+	@Test
+	void testHaveSecondWitnessValidLocation() {
+        Witness secondWitness = WitnessList.get(1);
+		boolean hasLocation = secondWitness.getLocation().equalsIgnoreCase("Hilton Hotel");
+		assertTrue(hasLocation);
+	}
+
+    @Test
+	void testHaveFirstWitnessValidType() {
+        Witness firstWitness = WitnessList.get(0);
+		boolean hasType = firstWitness.getTypeOfWitness().equalsIgnoreCase("Bystander");
+		assertTrue(hasType);
+	}
+
+	@Test
+	void testHaveSecondWitnessValidType() {
+        Witness secondWitness = WitnessList.get(1);
+		boolean hasType = secondWitness.getTypeOfWitness().equalsIgnoreCase("Bystander");
+		assertTrue(hasType);
+	}
+
+    @Test
+    void testForSearchNotInList() {
+        Witness firstWitness = WitnessList.get(0);
+        boolean hasType = firstWitness.getTypeOfWitness().equalsIgnoreCase("Not Here");
+        assertFalse(hasType);
+
+    }
+
+    @Test
+	void testLengthOfList()
+	{
+		assertEquals(WitnessList.size(), 2);
+	}
 
 }
