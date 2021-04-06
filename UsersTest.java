0ae79de581942fbd05cfdc2911f8ee833ee9b484
Kyle Persyn <kpersyn@email.sc.edu>
2021-04-06 00:01:20 -0400

    Completed Tests For UsersTest

--- UsersTest.java
+++ UsersTest.java
@@ -7,27 +7,61 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class UsersTest {
-	private Users users = Users.getInstance();
-	private ArrayList<User> userList = users.getUsersList();
+	
+	private ArrayList<User> userList = new ArrayList<User>();
 	
 	@BeforeEach
 	public void setup() {
 		userList.clear();
 		userList.add(new User("Kyle", "Kyle1234"));
 		userList.add(new User("Canaan", "Cnan2351"));
-		DataWriter.saveUsers();
 	}
 	
 	@AfterEach
 	public void tearDown() {
-		Users.getInstance().getUsersList().clear();
-		DataWriter.saveUsers();
+		userList.clear();
 	}
 	
 	
 	@Test
-	void testHaveUserValidFirstItem() {
-		boolean hasKyle = users.haveUser("Kyle");
+	void testHaveUserValidFirstUserName() {
+		boolean hasKyle = userList.get(0).getUsername().equalsIgnoreCase("Kyle");
 		assertTrue(hasKyle);
 	}
+
+	@Test
+	void testHaveUserValidSecondUserName() {
+		boolean hasCanaan = userList.get(1).getUsername().equalsIgnoreCase("Canaan");
+		assertTrue(hasCanaan);
+	}
+
+	@Test
+	void testHaveUserValidFirstPassword() {
+		boolean hasKylePW = userList.get(0).getPassword().equalsIgnoreCase("Kyle1234");
+		assertTrue(hasKylePW);
+	}
+	
+	@Test
+	void testHaveUserValidSecondPassword() {
+		boolean hasCanaanPW = userList.get(1).getPassword().equalsIgnoreCase("Cnan2351");
+		assertTrue(hasCanaanPW);
+	}
+
+	@Test
+	void testUsersListLength() {
+		assertEquals(userList.size(), 2);
+	}
+
+	@Test
+	void testHaveUserEmpty() {
+		boolean hasEmpty = userList.get(0).getUsername().equalsIgnoreCase("");
+		assertFalse(hasEmpty);
+	}
+	
+	@Test
+	void testHaveUserNull() {
+		boolean hasNull = userList.get(0).getUsername().equalsIgnoreCase(null);
+		assertFalse(hasNull);
+	}
+	
 }
