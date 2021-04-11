f8b45a7e604f3d0d5c95b11aefa1ade56c4bb20e
Kyle Persyn <kpersyn@email.sc.edu>
2021-04-11 17:00:04 -0400

    Added suspectslist test

--- suspectsListTest.java
+++ suspectsListTest.java
@@ -13,8 +13,7 @@ class suspectsListTest {
 	@BeforeEach
 	public void setup() {
 		SuspectsList.clear();  
-		SuspectsList.add(new Suspect("Kyle", "Persyn", 19, "Male", "white", contactInfo, isMinor, statement, hasAlibi2, birthDate, eyeColor, hairDescription, facialHairDescription, height, weight, tattooDescription, hasPiercings2, physicalMarksDescription, physicalBuild, weaponDescription, complexion, clothingDescription, vehicleDescription));
-        SuspectsList.add(new Victim("Canaan", "Eident", 20, "803-298-7924", "False", "True", "False", "Someone tried to rob me while I was pumping gas"));
+		SuspectsList.add(new Suspect("Kyle", "Persyn", 19, "Male", "white", "803-792-2294", "false", "I was at the field", "false", "July 24, 2001", "Green", "Short Brown Hair", "Beard", "5'11", "178", "No Tattoos", "None", "None", "Athletic", "None", "Smooth tan Complexion", "Athletic Clothes", "Black Chevy Avalanche"));
 		DataWriter.saveSuspect();
 	}
 	
@@ -23,4 +22,26 @@ class suspectsListTest {
 		suspectsList.getInstance().getSuspectList().clear();
 		DataWriter.saveSuspect();
 	}
+
+    @Test
+    void testHasValidFirstSuspect(){
+        boolean hasSuspect = suspects.searchSuspectsByName("Kyle", "Persyn") != null;
+        assertTrue(hasSuspect);
+    }
+
+    @Test
+    void testAddCriminal() {
+        Suspect canaan = new Suspect("Canaan", "Eident", 19, "Male", "white", "803-792-2294", "false", "I was at the field", "false", "July 24, 2001", "Green", "Short Brown Hair", "Beard", "5'11", "178", "No Tattoos", "None", "None", "Athletic", "None", "Smooth tan Complexion", "Athletic Clothes", "Black Chevy Avalanche");
+        suspects.addSuspect(canaan);
+        boolean hasSuspect = suspects.searchSuspectsByName("Canaan", "Eident") != null;
+        assertTrue(hasSuspect);
+    }
+
+    @Test
+    void testCreateDupe(){
+        Suspect dupe = new Suspect("Kyle", "Persyn", 19, "Male", "white", "803-792-2294", "false", "I was at the field", "false", "July 24, 2001", "Green", "Short Brown Hair", "Beard", "5'11", "178", "No Tattoos", "None", "None", "Athletic", "None", "Smooth tan Complexion", "Athletic Clothes", "Black Chevy Avalanche");
+        suspects.addSuspect(dupe);
+        assertNotEquals(suspects.getSuspectList().size(), 2);
+    }
+}
 	
